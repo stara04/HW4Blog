c52cd33c21816f246f183d59c9894cff3c4ff055
tara.kuruvilla@utexas.edu <tara.kuruvilla@utexas.edu>
2019-11-03 19:13:32 -0600

    button working sort of

--- client1/src/components/posts.component.js
+++ client1/src/components/posts.component.js
@@ -8,7 +8,41 @@ export default class Posts extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {posts: []};
+    this.state = {
+      posts: [],
+      signedIn: false,
+      user: ""
+    };
+
+    this.onSignIn = this.onSignIn.bind(this);
+  }
+
+  onSignIn(googleUser) {
+
+    if (process.env.NODE_ENV === "production"){
+      console.log("oops, going to the wrong place");
+      // Useful data for your client-side scripts:
+      var profile = googleUser.getBasicProfile();
+      console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+      let name = profile.getName();
+      console.log('Full Name: ' + profile.getName());
+      console.log("Email: " + profile.getEmail());
+
+      // The ID token you need to pass to your backend:
+      var id_token = googleUser.getAuthResponse().id_token;
+      console.log("ID Token: " + id_token);
+      this.setState({
+        signedIn: true,
+        user: name
+      });
+    } else{
+      this.setState({
+        signedIn: true,
+        user: "testUser1"
+      });
+      console.log("signed in!");
+    }
+
   }
 
   componentDidMount() {
@@ -30,7 +64,7 @@ export default class Posts extends Component {
   render() {
          return (
              <div>
-                 <div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
+                 <div class="g-signin2" onClick= {this.onSignIn} data-onsuccess="onSignIn" data-theme="dark"></div>
                  <h3>Our Posts</h3>
                 { this.getPosts() }
              </div>
