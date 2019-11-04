8f38ec3081677ec15c10ba45056a877c0d065713
Emily Ginsburg <emily.ginsburg@utexas.edu>
2019-11-03 19:06:31 -0600

    Added header

--- client1/src/App.js
+++ client1/src/App.js
@@ -4,7 +4,7 @@ import CreatePost from "./components/createpost.component";
 import Posts from "./components/posts.component";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
+import ourPicture from './our_picture.JPG';
 
 class App extends Component {
 state = {
@@ -32,12 +32,12 @@ state = {
     return (
       <Router>
         <div className="container">
+        <br/>
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link to="/" className="navbar-brand">Tara and Emily's Blog</Link>
             <div className="collpase navbar-collapse">
               <ul className="navbar-nav mr-auto">
                 <li className="navbar-item">
-                  <Link to="/" className="nav-link">Posts</Link>
+                  <Link to="/" className="nav-link">Home</Link>
                 </li>
                 <li className="navbar-item">
                   <Link to="/create" className="nav-link">Create New Post</Link>
@@ -46,6 +46,14 @@ state = {
             </div>
           </nav>
           <br/>
+          <div className="top-section">
+            <div className="blog-main-header">
+              <h1>Welcome to Our Blog</h1>
+              <div className="blog-main-desc">Hi there! We are Emily and Tara. We are seniors in EE461L this semester. We have been working on projects together since Freshman Year! We hope you enjoy our blog. Feel free to contribute (once you sign in) :)
+              </div>
+              </div>
+           <img src={ourPicture} className="App-logo" alt="logo" />
+          </div>
           <Route path="/" exact component={Posts} />
           <Route path="/create" component={CreatePost} />
         </div>
